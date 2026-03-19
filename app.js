29519e0efe48978dd5c910a540a12f8760d0961b
Yash Barahate <yashmeena00108@gmial.com>
2026-03-19 16:35:37 +0530

    mongo atlas

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine","ejs");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/intern-assignment"
+const MONGO_URL =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/intern-assignment";
 
+  
 main()
  .then(()=>{
     console.log("connected to DB");
